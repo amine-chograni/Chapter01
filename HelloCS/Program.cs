7d01bcdb8f222e03f5aa11d4dd794330049313a3
Amine CHOGRANI <aminechograni@gmail.com>
2026-04-15 21:26:45 +0100

    Print namespace and throw exception in Program.cs
    Added code to retrieve and display the Program class's namespace, defaulting to "<null>" if unavailable. Also introduced an Exception throw at the end to terminate execution with an error.

--- HelloCS/Program.cs
+++ HelloCS/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
+
+string name = typeof(Program).Namespace ?? "<null>";
+Console.WriteLine($"Namespace: {name}");
+
+throw new Exception();
